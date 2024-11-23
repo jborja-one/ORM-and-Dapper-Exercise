aaaf33b36a16437f37981a93cb2c0bb6388e1b87
Jonathan Borja <jborja.one@gmail.com>
2024-11-23 13:43:03 -0700

    done ex. 1

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ORM_Dapper
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;using MySql.Data.MySqlClient;namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
         }
     }
 }
