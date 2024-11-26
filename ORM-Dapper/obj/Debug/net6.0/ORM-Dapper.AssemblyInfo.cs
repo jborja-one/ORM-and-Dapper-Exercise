96caceca416990e36c42c62bd7a9fde594119a3c
Jonathan Borja <jborja.one@gmail.com>
2024-11-25 18:29:59 -0700

    done

--- ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
+++ ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6db1cd5293db68a3a8b8c09940954329a4355d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a48682bff22c318f5c92cf821f96064832d1652")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
