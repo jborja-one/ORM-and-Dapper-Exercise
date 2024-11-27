23e327e4964f6c27fe9947812be4504675cd2ab0
Jonathan Borja <81830502+jborja-one@users.noreply.github.com>
2024-11-27 11:04:10 -0700

    Update DapperProductRepository.cs
    update

--- ORM-Dapper/DapperProductRepository.cs
+++ ORM-Dapper/DapperProductRepository.cs
@@ -1,1 +1,48 @@
-﻿using System.Collections.Generic;using System.Data;using Dapper;namespace ORM_Dapper{    public class DapperProductRepository : IProductRepository    {        private readonly IDbConnection _connection;        public DapperProductRepository(IDbConnection connection )        {            _connection = connection;        }        public void CreateProduct(string name, double price, int categoryID)        {            _connection.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES(@name, @price, @categoryID);",            new {                name = name, price = price, categoryID = categoryID });        }        public IEnumerable<Product> GetAllProducts()        {            //dapper extends ==> IDbConnection            return _connection.Query<Product>("SELECT * FROM products;");        }        public void UpdateProduct(int productID, string updatedName)        {            _connection.Execute("UPDATE products SET Name = @updatedName WHERE ProductID = @productID;",                new {updatedName = updatedName, productID = productID});        }        public void DeleteProduct(int productID)        {            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",                new { productID = productID });            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;",                new { productID = productID });            _connection.Execute("DELETE FROM products WHERE ProductID = @productsID;",                new { productID = productID });        }    }}
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace ORM_Dapper
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection )
+        {
+            _connection = connection;
+        }
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES(@name, @price, @categoryID);",
+            new {
+                name = name, price = price, categoryID = categoryID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            //dapper extends ==> IDbConnection
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+
+        public void UpdateProduct(int productID, string updatedName)
+        {
+            _connection.Execute("UPDATE products SET Name = @updatedName WHERE ProductID = @productID;",
+                new {updatedName = updatedName, productID = productID});
+        }
+
+        public void DeleteProduct(int productID)
+        {
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;",
+                new { productID = productID });
+        }
+    }
+}
+
